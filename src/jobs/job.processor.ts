7568b6992b80ea598eda8e695b57d50f3c8bc295
叶子 <519403248@qq.com>
2021-05-25 17:25:56 +0800

    晚上sleep时间加倍

--- src/jobs/job.processor.ts
+++ src/jobs/job.processor.ts
@@ -6,6 +6,7 @@ import { RankService } from '../modules/rank/rank.service';
 import { RankId } from '../modules/rank/rank.entity';
 import { VideoService } from '../modules/video/video.service';
 import { UpService } from '../modules/up/up.service';
+import dayjs from 'dayjs';
 
 @Processor('job')
 export class JobProcessor {
@@ -18,25 +19,28 @@ export class JobProcessor {
   @Process('crawler')
   async crawlerWork(job: Job<JobData>) {
     const { key, type } = job.data;
+    const time = dayjs().get('hour');
+    const isMidNight = time < 6;
+    const $sleep = async (t: number) => await sleep(isMidNight ? t * 2 : t);
     switch (type) {
       case JobType.Rank:
         if (key === -1) {
           await this.rankService.count();
-          await sleep(1000);
+          await $sleep(1000);
         } else {
           await this.rankService.fetch(key as RankId);
-          await sleep(3000);
+          await $sleep(3000);
         }
         break;
       case JobType.Up:
         const mid = +key;
         if (isNaN(mid)) return;
         await this.upService.fetch(mid);
-        await sleep(10000);
+        await $sleep(10000);
         break;
       case JobType.Video:
         await this.videoService.fetch(key as string);
-        await sleep(3000);
+        await $sleep(3000);
         break;
     }
   }
