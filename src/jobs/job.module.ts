4f1c57ad6cabaeb7e25c568b6d3f986e9a3f2076
叶子 <519403248@qq.com>
2021-05-26 13:40:33 +0800

    job module

--- src/jobs/job.module.ts
+++ src/jobs/job.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { JobService } from './job.service';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
+  imports: [
+    BullModule.registerQueueAsync({
+      name: 'job',
+    }),
+  ],
   providers: [JobService],
 })
 export class JobModule {}
