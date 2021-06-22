58b42aafda0aa428a91719747949a85e14824fdf
叶子 <519403248@qq.com>
2021-06-22 14:44:45 +0800

    paused

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -26,7 +26,7 @@ export default class NodeAxios {
   interceptors() {
     this.axiosInstance.interceptors.request.use(async (req) => {
       const cookie =
-        "_uuid=1C7AA815-2BE3-2FD5-2F27-0A6CE36B579397591infoc; buvid3=99386200-1DE0-4D40-AEAA-5DB025952B70143079infoc; sid=lx53ui2n; fingerprint=51936f1d8205f677566b7ff40a403d47; CURRENT_FNVAL=80; blackside_state=1; CURRENT_QUALITY=120; rpdid=|(umYR)|JuJR0J'uYumu~uRmR; buvid_fp_plain=3DBE2F24-550A-B4C3-139B-8143C46CFE7656853infoc; DedeUserID=5213161; DedeUserID__ckMd5=426a9e940246e7aa; SESSDATA=23af0a8d%2C1635235928%2Cefe78*41; bili_jct=1fc58a4717dcff0b68eefdc7cb1d5767; bsource=search_baidu; LIVE_BUVID=AUTO3616210005492248; bp_video_offset_5213161=524672775390779107; bp_t_offset_5213161=524680609406987779; PVID=3";
+        "_uuid=1C7AA815-2BE3-2FD5-2F27-0A6CE36B579397591infoc; buvid3=99386200-1DE0-4D40-AEAA-5DB025952B70143079infoc; sid=lx53ui2n; CURRENT_FNVAL=80; blackside_state=1; CURRENT_QUALITY=120; rpdid=|(umYR)|JuJR0J'uYumu~uRmR; DedeUserID=5213161; DedeUserID__ckMd5=426a9e940246e7aa; LIVE_BUVID=AUTO3616210005492248; PVID=3; fingerprint=088f67aa9191883b76eec15427f855e8; buvid_fp=99386200-1DE0-4D40-AEAA-5DB025952B70143079infoc; buvid_fp_plain=99386200-1DE0-4D40-AEAA-5DB025952B70143079infoc; fingerprint=4f2a498c8206764bc95e74e997544b0f; buvid_fp=99386200-1DE0-4D40-AEAA-5DB025952B70143079infoc; buvid_fp_plain=225D964D-74FB-4E92-9D01-BA998947256834755infoc; SESSDATA=0a0b6e83%2C1639810888%2C3b6c5*61; bili_jct=c9117882ef555511387a0ab64eebe308";
       // (await $redis.str.get(['bilibili', 'cookie'].join(':')))[1] || '';
       req.headers = {
         ...req.headers,
